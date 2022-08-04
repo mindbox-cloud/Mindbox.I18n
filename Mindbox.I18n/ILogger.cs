80aa747e40f2e9b8cbd9856c1c84390dddcd72db
Beliakov Aleksei <beliakov@mindbox.ru>
2022-08-04 12:44:09 +0300

    Locale->ILocale

--- Mindbox.I18n/ILogger.cs
+++ Mindbox.I18n/ILogger.cs
@@ -5,9 +5,9 @@ namespace Mindbox.I18n;
 
 public interface ILogger
 {
-	void LogMissingNamespace(Locale locale, string @namespace, string key);
+	void LogMissingNamespace(ILocale locale, string @namespace, string key);
 	void LogMissingLocale(ILocale locale, string key);
-	void LogMissingKey(Locale locale, string @namespace, string key);
+	void LogMissingKey(ILocale locale, string @namespace, string key);
 	void LogInvalidKey(string key);
 	void LogInvalidOperation(string message);
 	void LogError(Exception exception, string message);
