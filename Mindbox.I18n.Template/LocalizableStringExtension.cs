735ebe43a33ddd264178b3060bfe687ed74830a9
Beliakov Aleksei <beliakov@mindbox.ru>
2022-08-08 09:39:17 +0300

    Separate I18n.Template and add LocalizableStringExtensions.RenderWithTemplate

---
+++ Mindbox.I18n.Template/LocalizableStringExtension.cs
@@ -0,0 +1,22 @@
+﻿using Mindbox.I18n.Abstractions;
+using Mindbox.Quokka;
+
+namespace Mindbox.I18n.Template;
+
+public static class LocalizableStringExtensions
+{
+	public static string RenderWithTemplate(
+		this ILocalizableString localizableString,
+		ILocalizationProvider localizationProvider,
+		ILocale locale,
+		ITemplateFactory templateFactory,
+		Dictionary<string, Func<string>>? parameters = null)
+	{
+		var @string = localizableString.Render(localizationProvider, locale);
+		if (parameters is null)
+			return @string;
+
+		var template = templateFactory.CreateTemplate(@string);
+		return template.Render(parameters.ToCompositeModelValue());
+	}
+}
