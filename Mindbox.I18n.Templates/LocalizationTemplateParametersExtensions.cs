bf82eb3fc07e3874c21987ef9900a74727166150
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-10-07 11:33:58 +0300

    Added LocalizationTemplateParametersExtensions

---
+++ Mindbox.I18n.Templates/LocalizationTemplateParametersExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Mindbox.I18n.Abstractions;
+using Mindbox.Quokka;
+
+namespace Mindbox.I18n.Template;
+
+public static class LocalizationTemplateParametersExtensions
+{
+	public static ICompositeModelValue ToCompositeModelValue(this LocalizationTemplateParameters parameters)
+	{
+		return new CompositeModelValue(parameters.Fields.Select(f => new ModelField(f.Key, f.Value)));
+	}
+}
