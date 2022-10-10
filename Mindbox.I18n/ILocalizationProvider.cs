3c4cba52f7e912514628f56112090504742125a6
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-10-10 09:45:28 +0300

    clean usings

--- Mindbox.I18n/ILocalizationProvider.cs
+++ Mindbox.I18n/ILocalizationProvider.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System.Runtime.CompilerServices;
 using Mindbox.I18n.Abstractions;
 
 namespace Mindbox.I18n;
