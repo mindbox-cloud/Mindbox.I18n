2edaf7799b212d0cad3affa8e5d947b7900acee0
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-10-28 15:42:52 +0300

    Added stubs and Moq

---
+++ Mindbox.I18n.Tests/TestsInfrastucture/CompositeModelDefinitionStub.cs
@@ -0,0 +1,37 @@
+﻿// Copyright 2022 Mindbox Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Mindbox.I18n.Tests;
+
+public class CompositeModelDefinitionStub : ICompositeModelDefinition
+{
+	public IReadOnlyDictionary<string, IModelDefinition> Fields { get; }
+
+	public IReadOnlyDictionary<IMethodCallDefinition, IModelDefinition> Methods =>
+		new Dictionary<IMethodCallDefinition, IModelDefinition>();
+
+	public CompositeModelDefinitionStub()
+	{
+		Fields = new Dictionary<string, IModelDefinition>()
+		{
+			["firstParam"] = Mock.Of<IModelDefinition>(),
+			["secondParam"] = Mock.Of<IModelDefinition>()
+		};
+	}
+
+	public CompositeModelDefinitionStub(IReadOnlyDictionary<string, IModelDefinition> fields)
+	{
+		Fields = fields ?? throw new ArgumentNullException(nameof(fields));
+	}
+}
---
+++ Mindbox.I18n.Tests/TestsInfrastucture/TemplateStub.cs
@@ -0,0 +1,45 @@
+﻿// Copyright 2022 Mindbox Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Mindbox.I18n.Tests;
+
+public class TemplateStub : ITemplate
+{
+	private readonly ICompositeModelDefinition _compositeModelDefinition;
+
+	public TemplateStub(ICompositeModelDefinition compositeModelDefinition)
+	{
+		_compositeModelDefinition = compositeModelDefinition ?? throw new ArgumentNullException(nameof(compositeModelDefinition));
+	}
+
+	public ICompositeModelDefinition GetModelDefinition() => _compositeModelDefinition;
+
+	public virtual string Render(
+		ICompositeModelValue model,
+		ICallContextContainer? callContextContainer = null)
+	{
+		throw new NotImplementedException();
+	}
+
+	public virtual void Render(
+		TextWriter textWriter,
+		ICompositeModelValue model,
+		ICallContextContainer? callContextContainer = null)
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool IsConstant => false;
+	public IList<ITemplateError>? Errors => null!;
+}
