002749d2b80628776c4e0d93f7a3c1d28eea79b6
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-10-28 12:18:38 +0300

    Added accept of template fields

---
+++ Mindbox.I18n.Templates/TemplateExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Mindbox.I18n.Abstractions;
+using Mindbox.Quokka;
+
+namespace Mindbox.I18n.Template;
+
+public static class TemplateExtensions
+{
+	public static bool Accepts(this ITemplate template, LocalizationTemplateParameters localizationTemplateParameters)
+	{
+		var requiredFields = template.GetModelDefinition().Fields;
+
+		if (requiredFields.Count != localizationTemplateParameters.Fields.Count)
+			return false;
+
+		foreach (var field in requiredFields)
+		{
+			if (!localizationTemplateParameters.Fields.ContainsKey(field.Key))
+				return false;
+		}
+
+		return true;
+	}
+}
