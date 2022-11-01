cce7588af710ee109af7e9700a8485d324839766
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-11-01 21:11:54 +0400

    Added extensions

--- Mindbox.I18n.Templates/LocalizationTemplateParametersExtensions.cs
+++ Mindbox.I18n.Templates/LocalizationTemplateParametersExtensions.cs
@@ -30,24 +30,13 @@ public static class LocalizationTemplateParametersExtensions
 			)));
 	}
 
-	public static IModelField ToModelField(this ParameterField value, ILocale locale)
+	public static LocalizationTemplateParameters WithField(
+		this LocalizationTemplateParameters localizationTemplateParameters,
+		string fieldName,
+		Func<ILocale, string> valueProvider)
 	{
-		return new ModelField(
-			value.Name,
-			value.Value.ToModelValue(locale)
-		);
+		return  localizationTemplateParameters
+			.WithField(fieldName, new PrimitiveParameter(valueProvider));
 	}
 
-	public static IModelValue ToModelValue(this ParameterValue value, ILocale locale) => value switch
-	{
-		CompositeParameter composite => composite.ToModelValue(locale),
-		PrimitiveParameter primitive => primitive.ToModelValue(locale),
-			_ => throw new InvalidCastException()
-	};
-
-	public static IModelValue ToModelValue(this PrimitiveParameter value, ILocale locale) =>
-		new PrimitiveModelValue(value.ValueProvider(locale));
-
-	public static IModelValue ToModelValue(this CompositeParameter value, ILocale locale) =>
-		new CompositeModelValue(value.Fields.Select(field => field.ToModelField(locale)));
 }
---
+++ Mindbox.I18n.Templates/ParameterValueExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Mindbox.I18n.Abstractions;
+using Mindbox.Quokka;
+
+namespace Mindbox.I18n.Template;
+
+public static class ParameterValueExtensions
+{
+	public static IModelField ToModelField(this ParameterField value, ILocale locale)
+	{
+		return new ModelField(
+			value.Name,
+			value.Value.ToModelValue(locale)
+		);
+	}
+
+	public static IModelValue ToModelValue(this ParameterValue value, ILocale locale) => value switch
+	{
+		CompositeParameter composite => composite.ToModelValue(locale),
+		PrimitiveParameter primitive => primitive.ToModelValue(locale),
+		_ => throw new InvalidCastException()
+	};
+
+	public static IModelValue ToModelValue(this PrimitiveParameter value, ILocale locale) =>
+		new PrimitiveModelValue(value.ValueProvider(locale));
+
+	public static IModelValue ToModelValue(this CompositeParameter value, ILocale locale) =>
+		new CompositeModelValue(value.Fields.Select(field => field.ToModelField(locale)));
+}
