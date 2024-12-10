6ec0a67ab49abe14e53bfe2e1927ee4e60dcdf1b
Ruslan Baysuakov <baysuakov@mindbox.cloud>
2024-12-10 10:36:12 +0300

    namespace fix

--- Mindbox.I18n.Abstractions/LocalizableEnumMemberAttribute.cs
+++ Mindbox.I18n.Abstractions/LocalizableEnumMemberAttribute.cs
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Mindbox.I18n.Abstractions;
-
-namespace Mindbox.I18n.Template;
+namespace Mindbox.I18n.Abstractions;
 
 [AttributeUsage(AttributeTargets.Field)]
 public sealed class LocalizableEnumMemberAttribute : Attribute
